050f7a4d337731606d551aa98c45bdb311fe3bb3
jebrax <batonez@gmail.com>
2020-11-03 08:06:23 +0300

    Separate detect/resolve collisions

--- game/collisions.s
+++ game/collisions.s
@@ -12,59 +12,96 @@
 
 .segment "CODE"
   .proc resolve_collisions
+    LDA #0
+    STA x_dist
+    STA y_dist
+
     LDA player_y
     CLC
     ADC player_size + 1
     CMP block_y
-    BCC skip_resolve
+    BCC end
 
     LDA block_y
     CLC
     ADC block_size + 1
     CMP player_y
-    BCC skip_resolve
+    BCC end
 
     LDA player_x
     CLC
     ADC player_size
     CMP block_x
-    BCC skip_resolve
+    BCC end
 
     LDA block_x
     CLC
     ADC block_size
     CMP player_x
-    BCC skip_resolve
+    BCC end
 
-      LDA block_y
-      CLC
-      ADC block_size + 1
-      SEC
-      SBC player_y
-      STA y_dist
+    caclulate_y_dist:
+    LDA block_y
+    CLC
+    ADC block_size + 1
+    SEC
+    SBC player_y
+    STA y_dist
 
-      LDA player_y
-      CLC
-      ADC player_size + 1
-      SEC
-      SBC block_y
-      CMP y_dist
-      BCS skip_overwrite
-      STA y_dist
+    LDA player_y
+    CLC
+    ADC player_size + 1
+    SEC
+    SBC block_y
+    CMP y_dist
+    BCS skip_overwrite_y
+    STA y_dist
+    skip_overwrite_y:
 
-      LDA player_y
-      SEC
-      SBC y_dist
-      STA player_y
-      CLV
-      BVC skip_resolve
+    caclulate_x_dist:
+    LDA block_x
+    CLC
+    ADC block_size
+    SEC
+    SBC player_x
+    STA x_dist
 
-      skip_overwrite:
-      LDA player_y
-      CLC
-      ADC y_dist
-      STA player_y
-    skip_resolve:
+    LDA player_x
+    CLC
+    ADC player_size
+    SEC
+    SBC block_x
+    CMP x_dist
+    BCS skip_overwrite_x
+    STA x_dist
+    skip_overwrite_x:
+
+    JSR resolve_collision
+
+    end:
+    RTS
+  .endproc
+
+  .proc resolve_collision
+    LDA x_dist
+    CMP y_dist
+    BCS resolve_y
+    
+    resolve_x:
+    LDA player_x
+    SEC
+    SBC x_dist
+    STA player_x
+    CLV
+    BVC end
+
+    resolve_y:
+    LDA player_y
+    SEC
+    SBC y_dist
+    STA player_y
+    
+    end:
     RTS
   .endproc
 
